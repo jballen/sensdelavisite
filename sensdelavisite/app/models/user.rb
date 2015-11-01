1a4d2297ab185e6ab0b323bbf2d932f5a9b49170
Brock Allen <jballen92@gmail.com>
2015-10-31 22:52:19 -0700

    removed email for now.

--- sensdelavisite/app/models/user.rb
+++ sensdelavisite/app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  # before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates(:name, presence: true, length: { maximum: 50 })
-  validates(:email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false })
+  # validates(:email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false })
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
-      user.email = auth.info.email
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
