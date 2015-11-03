ae941a4c83fe4fe57a632cf36352609d184d00e5
Brock Allen <jballen92@gmail.com>
2015-11-02 20:30:33 -0800

    basic styles and responsive design completed. Facebook sign in and out also completed.

--- sensdelavisite/config/initializers/omniauth.rb
+++ sensdelavisite/config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '801318119990108', '2b3402bc0a2b23d92a64ac695e4686a7', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  provider :facebook, '801587743296479', 'dd4b77f40d3c9c92f3322ff979c6acc4', 
+            :scope => "email", :info_fields => "name,email,image", :display => 'popup', 
+            :client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}
 end
--- sensdelavisite/db/schema.rb
+++ sensdelavisite/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101053206) do
+ActiveRecord::Schema.define(version: 20151101230147) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20151101053206) do
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
     t.string   "provider"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
